a335c0c487055b5133bdc95390b1bf864c01e1be
WzeeaSajid <wzeea.x@gmail.com>
2024-12-06 14:59:22 +0500

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace budgetCalculator
+{
+    partial class Login
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(137, 32);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(171, 22);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(137, 75);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(171, 22);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(34, 35);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(73, 16);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(34, 78);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(70, 16);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(69, 128);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(103, 32);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(194, 128);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 32);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "back";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 192);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ addAppliance.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace budgetCalculator
+{
+    partial class addAppliance
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtApplianceName = new System.Windows.Forms.TextBox();
+            this.numericUpDownWattage = new System.Windows.Forms.NumericUpDown();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblApplianceName = new System.Windows.Forms.Label();
+            this.lblWattage = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWattage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtApplianceName
+            // 
+            this.txtApplianceName.Location = new System.Drawing.Point(150, 30);
+            this.txtApplianceName.Name = "txtApplianceName";
+            this.txtApplianceName.Size = new System.Drawing.Size(200, 22);
+            this.txtApplianceName.TabIndex = 0;
+            // 
+            // numericUpDownWattage
+            // 
+            this.numericUpDownWattage.Location = new System.Drawing.Point(150, 70);
+            this.numericUpDownWattage.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownWattage.Name = "numericUpDownWattage";
+            this.numericUpDownWattage.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownWattage.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(275, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save Appliance";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblApplianceName
+            // 
+            this.lblApplianceName.AutoSize = true;
+            this.lblApplianceName.Location = new System.Drawing.Point(30, 30);
+            this.lblApplianceName.Name = "lblApplianceName";
+            this.lblApplianceName.Size = new System.Drawing.Size(111, 16);
+            this.lblApplianceName.TabIndex = 3;
+            this.lblApplianceName.Text = "Appliance Name:";
+            // 
+            // lblWattage
+            // 
+            this.lblWattage.AutoSize = true;
+            this.lblWattage.Location = new System.Drawing.Point(30, 70);
+            this.lblWattage.Name = "lblWattage";
+            this.lblWattage.Size = new System.Drawing.Size(61, 16);
+            this.lblWattage.TabIndex = 4;
+            this.lblWattage.Text = "Wattage:";
+            // 
+            // addAppliance
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.lblWattage);
+            this.Controls.Add(this.lblApplianceName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.numericUpDownWattage);
+            this.Controls.Add(this.txtApplianceName);
+            this.Name = "addAppliance";
+            this.Text = "Add Appliance";
+            this.Load += new System.EventHandler(this.ApplianceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWattage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtApplianceName;
+        private System.Windows.Forms.NumericUpDown numericUpDownWattage;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblApplianceName;
+        private System.Windows.Forms.Label lblWattage;
+    }
+}
